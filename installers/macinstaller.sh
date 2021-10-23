0387dd46b2fac490091eb650ba9228bc7ba385bb
Alex <madroadstudio@gmail.com>
2021-10-23 23:40:49 +1100

    Got rid of oldpwd, as variables aren't set properly and sorta useless

--- installers/macinstaller.sh
+++ installers/macinstaller.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-oldpwd = pwd
 echo "Make sure to install 'phoenixminer python3 git and nano' "
 cd ~/Library/Preferences/
 git clone https://github.com/MadRoadStudio/Phoenix-Assist
 cd Phoenix-Assist
 sudo cp -a mac-bin/* /usr/local/bin/
-cd oldpwd
 echo "Installed! Use 'mine' to start."
