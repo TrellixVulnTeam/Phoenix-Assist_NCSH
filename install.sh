9afc817a94deda93a729c0eb9c9700af643bc8a3
Alex <madroadstudio@gmail.com>
2021-10-23 23:18:11 +1100

    Delete install.sh (legacy for linux)

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Make sure to install 'phoenixminer python3 git' "
-cd ~/Documents
-git clone https://github.com/MadRoadStudio/Phoenix-Assist
-cd Phoenix-Assist/bin
-echo "export PATH=$PWD:\$PATH" >> ~/.bashrc
-echo "Installed! Use 'mine' to start."
